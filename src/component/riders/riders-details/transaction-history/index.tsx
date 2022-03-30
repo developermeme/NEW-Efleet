9e3e47357c8e9435ebbc3f35ffc10eeb88364e98
arunSurya-2812 <81164749+arunSurya-2812@users.noreply.github.com>
2022-03-30 17:32:48 +0530

    rider payment details download as a excel sheet done in rider component

--- src/component/riders/riders-details/transaction-history/index.tsx
+++ src/component/riders/riders-details/transaction-history/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import useRiders from "../../useRiders";
+import { TextButton } from "../../../../ui-kit/TextButton/TextButton.view";
+import * as XLSX from "xlsx";
 
 export const Transaction = () => {
   let TranscationHeaders = [
@@ -16,6 +18,13 @@ export const Transaction = () => {
     fetchTransactionDetail();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const ExcelSheet = (transactionDetails: any) => {
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(transactionDetails);
+    XLSX.utils.book_append_sheet(wb, ws, `${transactionDetails[0].name}sheet`);
+    XLSX.writeFile(wb, `${transactionDetails[0].name}.xlsx`);
+  };
 
   return (
     <>
@@ -34,19 +43,29 @@ export const Transaction = () => {
           <tbody>
             {transactionDetails?.map((paid: any, index: any) => {
               return (
-                <tr className="tableBody" key={index}>
-                  <td data-label="RiderId">{paid.riderId} </td>
-                  <td data-label="Name">{paid.name}</td>
-                  <td data-label="Pending">{paid.amountpaid || "N/A"}</td>
-                  <td data-label="lastpaid">{paid.lastpaid || "N/A"}</td>
-                  <td data-label="amountpaid">
-                    {paid.pending || Math.round(0)}
-                  </td>
-                </tr>
+                <>
+                  <tr className="tableBody" key={index}>
+                    <td data-label="RiderId">{paid.riderId} </td>
+                    <td data-label="Name">{paid.name}</td>
+                    <td data-label="Pending">{paid.amountpaid || "N/A"}</td>
+                    <td data-label="lastpaid">{paid.lastpaid || "N/A"}</td>
+                    <td data-label="amountpaid">
+                      {paid.pending || Math.round(0)}
+                    </td>
+                  </tr>
+                </>
               );
             })}
           </tbody>
         </table>
+      </div>
+      <br />
+      <div className="button-wrapper">
+        <TextButton
+          onClick={() => ExcelSheet(transactionDetails)}
+          items={<>Export Report</>}
+          id="add-zone"
+        />
       </div>
     </>
   );
